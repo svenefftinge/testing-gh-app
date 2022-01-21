ee4fe319c40ac62f7a1a343ad998eb83a23a8527
Sven Efftinge <sven@gitpod.io>
2022-01-21 11:29:14 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,4 @@ dd
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();323e3
+reportWebVitals();323
