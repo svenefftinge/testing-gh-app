b48fcb79ddd4db67bbb7c2ceac0ad0053833269e
Sven Efftinge <sven@gitpod.io>
2022-01-21 15:18:02 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -14,4 +14,4 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(); $%§$&%$&%/$&/ weew234234
+reportWebVitals(); $%§$&%$&%/$&/ weew234
